1e9ffc76e3861120ef779c00c8fcc7e09eaa290f
Jakub Filipowicz <jakubf@gmail.com>
2020-12-18 20:22:11 +0100

    port speed is defined by the f/w

--- tool/ictester.py
+++ tool/ictester.py
@@ -17,7 +17,6 @@ if '--list' in sys.argv:
 
 parser = argparse.ArgumentParser(description='IC tester controller')
 parser.add_argument('--device', default="/dev/ttyUSB1", help='Serial port where the IC tester is connected')
-parser.add_argument('--speed', default="19200", help='Port speed')
 parser.add_argument('--loop_pow', default=10, type=int, choices=range(0, 16), help='Loop count power (2^loop_pow)')
 parser.add_argument('--list', action="store_true", help='List all supported parts')
 parser.add_argument('--debug', action="store_true", help='Enable debug output')
@@ -32,7 +31,7 @@ except KeyError:
     sys.exit(1)
 
 print("Testing {}-pin part {}: {}, package {}...".format(part.pincount, part.name, part.desc, part.package_name))
-tester = Tester(part, args.device, args.speed, debug=args.debug)
+tester = Tester(part, args.device, 500000, debug=args.debug)
 for test_name in tester.tests_available():
     if not args.debug:
         print("Running test: {}... ".format(test_name), end='', flush=True)
