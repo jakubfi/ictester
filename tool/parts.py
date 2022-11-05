baada069e895384ca85b053f1fff9892481bda83
Jakub Filipowicz <jakubf@gmail.com>
2022-11-05 22:52:32 +0100

    add 7489

--- tool/parts.py
+++ tool/parts.py
@@ -927,6 +927,65 @@ class Part7486(PartDIP14):
 
 
 # ------------------------------------------------------------------------
+class Part7489(PartDIP16):
+    name = "7489"
+    desc = "64-bit random-access read/write memory"
+    pin_cfg = {
+        1: Pin("A0", Pin.INPUT),
+        2: Pin("~ME", Pin.INPUT),
+        3: Pin("~WE", Pin.INPUT),
+        4: Pin("D1", Pin.INPUT),
+        5: Pin("~Q1", Pin.OC),
+        6: Pin("D2", Pin.INPUT),
+        7: Pin("~Q2", Pin.OC),
+        9: Pin("~Q3", Pin.OC),
+        10: Pin("D3", Pin.INPUT),
+        11: Pin("~Q4", Pin.OC),
+        12: Pin("D4", Pin.INPUT),
+        13: Pin("A3", Pin.INPUT),
+        14: Pin("A2", Pin.INPUT),
+        15: Pin("A1", Pin.INPUT),
+    }
+    # ------------------------------------------------------------------------
+    def mem_rw_test_gen():
+        # --------------------------------------------------------------------
+        def rw_cycle(addr_vec):
+            return [
+                # write 1s
+                [addr_vec + [1, 1, 1, 1,  1, 1], [1, 1, 1, 1]],  # NONE: ou = 1
+                [addr_vec + [1, 1, 1, 1,  0, 0], [0, 0, 0, 0]],  # WRITE: ou = ~in
+                [addr_vec + [1, 1, 1, 1,  1, 1], [1, 1, 1, 1]],  # NONE: ou = 1
+                [addr_vec + [0, 0, 0, 0,  1, 0], [1, 1, 1, 1]],  # WR INHIBIT (with data inputs swapped): ou = ~in
+                # read 1s
+                [addr_vec + [1, 1, 1, 1,  0, 1], [0, 0, 0, 0]],  # READ: ou = ~mem word
+                [addr_vec + [1, 1, 1, 1,  1, 1], [1, 1, 1, 1]],  # NONE ou = 1
+                # write 0s
+                [addr_vec + [0, 0, 0, 0,  1, 1], [1, 1, 1, 1]],  # NONE: ou = 1
+                [addr_vec + [0, 0, 0, 0,  0, 0], [1, 1, 1, 1]],  # WRITE: ou = ~in
+                [addr_vec + [0, 0, 0, 0,  1, 1], [1, 1, 1, 1]],  # NONE: ou = 1
+                [addr_vec + [1, 1, 1, 1,  1, 0], [0, 0, 0, 0]],  # WR INHIBIT (with data inputs swapped): ou = ~in
+                # read 0s
+                [addr_vec + [0, 0, 0, 0,  0, 1], [1, 1, 1, 1]],  # READ: ou = ~mem word
+                [addr_vec + [0, 0, 0, 0,  1, 1], [1, 1, 1, 1]],  # NONE: ou = 1
+            ]
+
+        body = []
+        for v in Test.binary_combinator(4):
+            body.extend(rw_cycle(v))
+
+        return Test(
+            name="Complete array",
+            inputs=[1, 15, 14, 13,  4, 6, 10, 12,  2, 3],
+            outputs=[5, 7, 9, 11],
+            ttype=Test.COMB,
+            body=body,
+            loops=256,
+        )
+
+    tests = [mem_rw_test_gen()]
+
+
+# ------------------------------------------------------------------------
 class Part7490(PartDIP14_vcc5):
     name = "7490"
     desc = "Decade counter"
