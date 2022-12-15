bb7d8c0141ecee152c6242b3e6fd3df2c26729e7
Jakub Filipowicz <jakubf@gmail.com>
2022-12-15 20:17:04 +0100

    add 7460

--- tool/parts.py
+++ tool/parts.py
@@ -712,17 +712,54 @@ class Part7453(PartDIP14):
     tests = [test_async]
 
 
+'''
+NOTE on testing expanders:
+~X and X are outputs, but not in a TTL-levels sense: ~X is output transistor collector, X is its emmiter.
+Pin configuration does the following:
+ * pulls up the collector with the internal pull-up resistor (OC output)
+ * connects the emmiter to a current sink (output driven low)
+Thus, ~X becomes the actual output, and X needs to be always driven low.
+'''
+
+# ------------------------------------------------------------------------
+class Part7460(PartDIP14):
+    name = "7460"
+    desc = "Dual 4-input expanders"
+    pin_cfg = {
+        1: Pin("1A", Pin.IN),
+        2: Pin("1B", Pin.IN),
+        3: Pin("1C", Pin.IN),
+        4: Pin("2A", Pin.IN),
+        5: Pin("2B", Pin.IN),
+        6: Pin("2C", Pin.IN),
+        8: Pin("2D", Pin.IN),
+        9: Pin("~2X", Pin.OC),
+        10: Pin("2X", Pin.IN),
+        11: Pin("1X", Pin.IN),
+        12: Pin("~1X", Pin.OC),
+        13: Pin("1D", Pin.IN),
+    }
+    test_async = Test(
+        name="Asynchronous operation",
+        inputs=[11, 10,  1, 2, 3, 13,  4, 5, 6, 8],
+        outputs=[12, 9],
+        ttype=Test.COMB,
+        loops = 64,
+        body = [
+            [
+                [0, 0] + i + i,
+                [not (i[0] & i[1] & i[2] & i[3]), not (i[0] & i[1] & i[2] & i[3])]
+            ]
+            for i in Test.binary_combinator(4)
+        ]
+    )
+    tests = [test_async]
+
+
 # ------------------------------------------------------------------------
 class Part7462(PartDIP14):
     name = "7462"
     desc = "AND-OR Gate Expander"
-    '''
-    ~X and X are outputs, but not in a TTL-levels sense: ~X is output transistor collector, X is its emmiter.
-    Pin configuration does the following:
-     * pulls up the collector with the internal pull-up resistor (OC output)
-     * connects the emmiter to a current sink (output driven low)
-    Thus, ~X becomes the actual output, and X needs to be always driven low.
-    '''
     pin_cfg = {
         1: Pin("A", Pin.IN),
         2: Pin("B", Pin.IN),
@@ -737,7 +774,6 @@ class Part7462(PartDIP14):
         12: Pin("I", Pin.IN),
         13: Pin("J", Pin.IN),
     }
-    missing = "Gate expansion is not tested"
     test_async = Test(
         name="Asynchronous operation",
         inputs=[8,  1, 2,  3, 4, 5,  9, 10, 11,  12, 13],
