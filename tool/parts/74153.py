6a4d8d09e1d6f1ef1a56143229986b59efca513f
Jakub Filipowicz <jakubf@gmail.com>
2022-12-18 17:02:03 +0100

    simplify 74153

--- tool/parts/74153.py
+++ tool/parts/74153.py
@@ -19,32 +19,30 @@ class Part74153(PackageDIP16):
         14: Pin("A", Pin.IN),
         15: Pin("~2G", Pin.IN),
     }
-    test_all = Test(
-        name="Complete logic",
-        inputs=[2, 14, 1, 3, 4, 5, 6, 15, 13, 12, 11, 10],
-        outputs=[7, 9],
-        ttype=Test.COMB,
+    default_inputs=[2, 14,  1,  3, 4, 5, 6,  15,  13, 12, 11, 10]
+    default_outputs=[7, 9]
+    test_select_0 = Test("Select 0", Test.COMB, default_inputs, default_outputs,
         body=[
-            # output is always "0" when G is high
-            [[0, 0,  1,  0, 0, 0, 0,  1,  0, 0, 0, 0], [0, 0]],
-            [[0, 1,  1,  0, 0, 0, 0,  1,  0, 0, 0, 0], [0, 0]],
-            [[1, 0,  1,  0, 0, 0, 0,  1,  0, 0, 0, 0], [0, 0]],
-            [[1, 1,  1,  0, 0, 0, 0,  1,  0, 0, 0, 0], [0, 0]],
-            [[0, 0,  1,  1, 1, 1, 1,  1,  1, 1, 1, 1], [0, 0]],
-            [[0, 1,  1,  1, 1, 1, 1,  1,  1, 1, 1, 1], [0, 0]],
-            [[1, 0,  1,  1, 1, 1, 1,  1,  1, 1, 1, 1], [0, 0]],
-            [[1, 1,  1,  1, 1, 1, 1,  1,  1, 1, 1, 1], [0, 0]],
-
-            # selection if G is low
-            [[0, 0,  0,  1, 1, 1, 0,  0,  1, 1, 1, 0], [0, 0]],
-            [[0, 1,  0,  1, 1, 0, 1,  0,  1, 1, 0, 1], [0, 0]],
-            [[1, 0,  0,  1, 0, 1, 1,  0,  1, 0, 1, 1], [0, 0]],
-            [[1, 1,  0,  0, 1, 1, 1,  0,  0, 1, 1, 1], [0, 0]],
-
-            [[0, 0,  0,  0, 0, 0, 1,  0,  0, 0, 0, 1], [1, 1]],
-            [[0, 1,  0,  0, 0, 1, 0,  0,  0, 0, 1, 0], [1, 1]],
-            [[1, 0,  0,  0, 1, 0, 0,  0,  0, 1, 0, 0], [1, 1]],
-            [[1, 1,  0,  1, 0, 0, 0,  0,  1, 0, 0, 0], [1, 1]],
+            [Test.bin2vec(addr, 2) + 2*([0] + Test.bin2vec(~(1<<addr), 4)), [0, 0]]
+            for addr in range(0, 4)
         ]
     )
-    tests = [test_all]
+    test_select_1 = Test("Select 1", Test.COMB, default_inputs, default_outputs,
+        body=[
+            [Test.bin2vec(addr, 2) + 2*([0] + Test.bin2vec(1<<addr, 4)), [1, 1]]
+            for addr in range(0, 4)
+        ]
+    )
+    test_inhibit_0 = Test("Inhibit 0", Test.COMB, default_inputs, default_outputs,
+        body=[
+            [addr + 2*[1,  0, 0, 0, 0], [0, 0]]
+            for addr in Test.binary_combinator(2)
+        ]
+    )
+    test_inhibit_1 = Test("Inhibit 1", Test.COMB, default_inputs, default_outputs,
+        body=[
+            [addr + 2*[1,  1, 1, 1, 1], [0, 0]]
+            for addr in Test.binary_combinator(2)
+        ]
+    )
+    tests = [test_select_0, test_select_1, test_inhibit_0, test_inhibit_1]
