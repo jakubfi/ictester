bdc1aca6a7c1c56a7b0253d14a0bfdb745c68014
Jakub Filipowicz <jakubf@gmail.com>
2023-04-28 17:34:46 +0200

    update/fix 7490

--- tool/parts/7490.py
+++ tool/parts/7490.py
@@ -8,8 +8,8 @@ class Part7490(PackageDIP14_vcc5):
         2: Pin("R0(1)", Pin.IN),
         3: Pin("R0(2)", Pin.IN),
         4: Pin("NC", Pin.NC),
-        6: Pin("R9-1", Pin.IN),
-        7: Pin("R9-2", Pin.IN),
+        6: Pin("R9(1)", Pin.IN),
+        7: Pin("R9(2)", Pin.IN),
         8: Pin("QC", Pin.OUT),
         9: Pin("QB", Pin.OUT),
         11: Pin("QD", Pin.OUT),
@@ -19,44 +19,50 @@ class Part7490(PackageDIP14_vcc5):
     }
 
     default_inputs = [2, 3,  6, 7,  14, 1]
-    default_outputs = [12, 11, 8, 9]
+    default_outputs = [11, 8, 9, 12]
 
-    test_resets = Test("Resets", Test.COMB, default_inputs, default_outputs,
-        body=[
-            # resets
-            [[1, 1,  0, 0,  0, 0], [0, 0, 0, 0]],
-            [[1, 1,  0, 1,  0, 0], [0, 0, 0, 0]],
-            [[0, 0,  1, 1,  0, 0], [1, 0, 0, 1]],
-            [[0, 1,  1, 1,  0, 0], [1, 0, 0, 1]],
-            [[1, 0,  1, 1,  0, 0], [1, 0, 0, 1]],
-            [[1, 1,  1, 1,  0, 0], [1, 0, 0, 1]],
-        ]
-    )
     test_count_cka = Test("Count CKA", Test.SEQ, default_inputs, default_outputs,
         body=[
-            # reset
-            [[1, 1,  0, 0,  0, 0], [0, 0, 0, 0]],
+            # reset R0
+            [[1, 1,  1, 0,  0, 0], [0, 0, 0, 0]],
             # count CKA
-            [[0, 0,  0, 0,  '-', 0], [1, 0, 0, 0]],
+            [[0, 0,  0, 0,  '-', 0], [0, 0, 0, 1]],
             [[0, 0,  0, 0,  '-', 0], [0, 0, 0, 0]],
-            [[0, 0,  0, 0,  '-', 0], [1, 0, 0, 0]],
+            [[0, 0,  0, 0,  '-', 0], [0, 0, 0, 1]],
         ]
     )
     test_count_ckb = Test("Count CKB", Test.SEQ, default_inputs, default_outputs,
         body=[
-            #reset
-            [[1, 1,  0, 0,  0, 0], [0, 0, 0, 0]],
+            # reset R0
+            [[1, 1,  1, 0,  0, 0], [0, 0, 0, 0]],
             # count CKB
-            [[0, 0,  0, 0,  0, '-'], [0, 0, 0, 1]],
             [[0, 0,  0, 0,  0, '-'], [0, 0, 1, 0]],
-            [[0, 0,  0, 0,  0, '-'], [0, 0, 1, 1]],
             [[0, 0,  0, 0,  0, '-'], [0, 1, 0, 0]],
+            [[0, 0,  0, 0,  0, '-'], [0, 1, 1, 0]],
+            [[0, 0,  0, 0,  0, '-'], [1, 0, 0, 0]],
             [[0, 0,  0, 0,  0, '-'], [0, 0, 0, 0]],
             # count CKB again to fill bits with 1s
-            [[0, 0,  0, 0,  0, '-'], [0, 0, 0, 1]],
             [[0, 0,  0, 0,  0, '-'], [0, 0, 1, 0]],
+            [[0, 0,  0, 0,  0, '-'], [0, 1, 0, 0]],
+            [[0, 0,  0, 0,  0, '-'], [0, 1, 1, 0]],
+        ]
+    )
+    test_resets = Test("Resets", Test.SEQ, default_inputs, default_outputs,
+        body=[
+            # reset R0
+            [[1, 1,  1, 0,  0, 0], [0, 0, 0, 0]],
+            # count 1 CKA
+            [[0, 0,  0, 0,  '-', 0], [0, 0, 0, 1]],
+            # count 1 CKB
             [[0, 0,  0, 0,  0, '-'], [0, 0, 1, 1]],
+            # no reset
+            [[0, 1,  0, 1,  0, 0], [0, 0, 1, 1]],
+            [[1, 0,  1, 0,  0, 0], [0, 0, 1, 1]],
+            # reset R9
+            [[1, 1,  1, 1,  0, 0], [1, 0, 0, 1]],
+            # reset R0
+            [[1, 1,  0, 0,  0, 0], [0, 0, 0, 0]],
         ]
     )
 
-    tests = [test_resets, test_count_cka, test_count_ckb]
+    tests = [test_count_cka, test_count_ckb, test_resets]
