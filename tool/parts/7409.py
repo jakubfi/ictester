674d2e88c95c8629a8ad5fd8e10d3d583862f0e9
Jakub Filipowicz <jakubf@gmail.com>
2023-08-30 16:14:03 +0200

    add 7409

---
+++ tool/parts/7409.py
@@ -0,0 +1,21 @@
+from binvec import BV
+from prototypes import (partimport, Pin, PinType, Test)
+
+class Part7409(partimport("7408")):
+    name = "7409"
+    desc = "Quad 2-input positive-AND gates with open-collector outputs"
+    pin_cfg = {
+        1: Pin("1A", PinType.IN),
+        2: Pin("1B", PinType.IN),
+        3: Pin("1Y", PinType.OC),
+        4: Pin("2A", PinType.IN),
+        5: Pin("2B", PinType.IN),
+        6: Pin("2Y", PinType.OC),
+        8: Pin("3Y", PinType.OC),
+        9: Pin("3A", PinType.IN),
+        10: Pin("3B", PinType.IN),
+        11: Pin("4Y", PinType.OC),
+        12: Pin("4A", PinType.IN),
+        13: Pin("4B", PinType.IN),
+    }
+
