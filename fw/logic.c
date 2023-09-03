b632e44b939bddef598e1017932a57beef7ccea5
Jakub Filipowicz <jakubf@gmail.com>
2023-09-03 15:28:21 +0200

    janitorial

--- fw/logic.c
+++ fw/logic.c
@@ -29,32 +29,30 @@ uint8_t handle_vectors_load(uint8_t pin_count)
 {
 	vectors_count = serial_rx_16le();
 
-	// receive all vectors
+	// receive all vectors (bits in DUT pin order))
 	for (uint16_t pos=0 ; pos<vectors_count ; pos++) {
 		for (uint8_t i=0 ; i<pin_count ; i+=8) {
 			vectors[pos][i/8] = serial_rx_char();
 		}
 	}
 
-	// reorder bits in each vector to match port connections
+	// reorder bits in each vector into MCU port pin order
 	for (uint16_t pos=0 ; pos<vectors_count ; pos++) {
 		// convert received bytes into temporary 32-bit number
 		uint32_t bitvector = 0;
 		for (uint8_t i=0 ; i<pin_count ; i+=8) {
-			bitvector |= (uint32_t)vectors[pos][i/8] << (i);
+			bitvector |= (uint32_t) vectors[pos][i/8] << i;
 		}
 		// clear received vector data
 		for (uint8_t i=0 ; i<3 ; i++) {
 			vectors[pos][i] = 0;
 		}
-		// fill in bits in correct positions
-		for (uint8_t pin=0 ; pin<pin_count ; pin++) {
-			uint8_t zif_pin = zif_pos(pin_count, pin);
+		// fill in bits in MCU port pin order
+		for (uint8_t dut_pin=0 ; dut_pin<pin_count ; dut_pin++) {
+			uint8_t zif_pin = zif_pos(pin_count, dut_pin);
 			int8_t port_pos = mcu_port(zif_pin);
-			if (port_pos >= 0) {
-				uint8_t bit_val = (bitvector >> pin) & 1;
-				vectors[pos][port_pos] |= bit_val << mcu_port_pin(zif_pin);
-			}
+			uint8_t bit_val = (bitvector >> dut_pin) & 1;
+			vectors[pos][port_pos] |= bit_val << mcu_port_pin(zif_pin);
 		}
 	}
 
@@ -68,10 +66,7 @@ static inline uint8_t run_logic2(uint16_t delay)
 		PORTB = ((vectors[pos][1] & port[1].dut_input) | port[1].dut_pullup);
 		PORTC = ((vectors[pos][2] & port[2].dut_input) | port[2].dut_pullup);
 
-		if (delay) {
-			uint16_t i = delay; // has to be redeclared, otherwise everything gets optimized away
-			_delay_loop_2(i);
-		}
+		if (delay) _delay_loop_2(delay);
 
 		if ((test_type == TYPE_COMB) || (pos % 2)) {
 			if ((PINB ^ vectors[pos][1]) & port[1].used_outputs) return RESP_FAIL;
@@ -90,10 +85,7 @@ static inline uint8_t run_logic3(uint16_t delay)
 		PORTB = ((vectors[pos][1] & port[1].dut_input) | port[1].dut_pullup);
 		PORTC = ((vectors[pos][2] & port[2].dut_input) | port[2].dut_pullup);
 
-		if (delay) {
-			uint16_t i = delay; // has to be redeclared, otherwise everything gets optimized away
-			_delay_loop_2(i);
-		}
+		if (delay) _delay_loop_2(delay);
 
 		if ((test_type == TYPE_COMB) || (pos % 2)) {
 			if ((PINA ^ vectors[pos][0]) & port[0].used_outputs) return RESP_FAIL;
@@ -108,16 +100,15 @@ static inline uint8_t run_logic3(uint16_t delay)
 // -----------------------------------------------------------------------
 uint8_t run_logic(uint16_t loops, uint8_t *params)
 {
-	uint8_t res;
 	volatile uint16_t delay = (params[1] << 8) + params[0];
 
 	if (pin_count <= 16) {
 		for (uint16_t rep=0 ; rep<loops ; rep++) {
-			if ((res = run_logic2(delay)) != RESP_PASS) return res;
+			if (run_logic2(delay) != RESP_PASS) return RESP_FAIL;
 		}
 	} else {
 		for (uint16_t rep=0 ; rep<loops ; rep++) {
-			if ((res = run_logic3(delay)) != RESP_PASS) return res;
+			if (run_logic3(delay) != RESP_PASS) return RESP_FAIL;
 		}
 	}
 
