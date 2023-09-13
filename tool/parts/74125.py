46ea68b591ad2248fca034975a9dac2d5ca8171e
Jakub Filipowicz <jakubf@gmail.com>
2023-09-13 07:58:20 +0200

    update 74125

--- tool/parts/74125.py
+++ tool/parts/74125.py
@@ -18,18 +18,39 @@ class Part74125(PackageDIP14):
         12: Pin("4A", PinType.IN),
         13: Pin("~4G", PinType.IN),
     }
+
     # switching to HiZ with 5k pullups requires some time
     read_delay_us = 2
 
-    test_all = Test("Complete logic", Test.LOGIC,
+    test_enabled = Test("Outputs enabled", Test.LOGIC,
+        inputs=[1, 4, 10, 13,  2, 5, 9, 12],
+        outputs=[3, 6, 8, 11],
+        body=[
+            [[0, 0, 0, 0, *v], v]
+            for v in BV.range(0, 2**4)
+        ]
+    )
+
+    test_disabled = Test("Outputs disabled", Test.LOGIC,
         params=list(round(read_delay_us/0.2).to_bytes(2, 'little')),
         inputs=[1, 4, 10, 13,  2, 5, 9, 12],
         outputs=[3, 6, 8, 11],
         body=[
-            [[*ena, *v], v|ena]
+            [[1, 1, 1, 1, *v], [1, 1, 1, 1]]
+            for v in BV.range(0, 2**4)
+        ]
+    )
+
+    test_switching = Test("Switching outputs", Test.LOGIC,
+        params=list(round(read_delay_us/0.2).to_bytes(2, 'little')),
+        loops=512,
+        inputs=[1, 4, 10, 13,  2, 5, 9, 12],
+        outputs=[3, 6, 8, 11],
+        body=[
+            [[*ena, *v], v | ena]
             for v in BV.range(0, 2**4)
             for ena in BV.range(0, 2**4)
         ]
     )
 
-    tests = [test_all]
+    tests = [test_enabled, test_disabled, test_switching]
